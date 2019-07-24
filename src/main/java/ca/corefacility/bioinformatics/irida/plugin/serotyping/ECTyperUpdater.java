44e00b455137431ad1870852e28330c88ca451dd
kbessonov1984 <kyrylo.bessonov@canada.ca>
2019-07-24 19:53:39 +0000

    Added sample meta data update functionality with the IRIDA wrapper version matching the ECTYPER version. Also accounted for the improver tab-delimited ECTYPER report format whare some columns are missing. Thus, opted to output result in the text file as otherwise some columns of the report were missing.

--- src/main/java/ca/corefacility/bioinformatics/irida/plugin/serotyping/ECTyperUpdater.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/plugin/serotyping/ECTyperUpdater.java
@@ -36,13 +36,13 @@ import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.service.workflow.IridaWorkflowsService;
 
 /**
- * {@link AnalysisSampleUpdater} for AMR detection results to be written to
+ * {@link AnalysisSampleUpdater} for ECTYPER results to be written to
  * metadata of {@link Sample}s.
  */
 public class ECTyperUpdater implements AnalysisSampleUpdater {
 	private static final Logger logger = LoggerFactory.getLogger(ECTyperUpdater.class);
 	/*private static final String STARAMR_SUMMARY = "staramr-summary.tsv";*/
-	private static final String ECTYPER_SUMMARY = "report_ectyper.tsv";
+	private static final String ECTYPER_SUMMARY = "serotyping report"; /*got to match the value in the irida_workflow.xml file <output name="serotyping report" fileName="report_ectyper.tsv"/>*/
 	private static final Splitter SPLITTER = Splitter.on('\t');
 	
 
@@ -75,8 +75,8 @@ public class ECTyperUpdater implements AnalysisSampleUpdater {
 	 *private AMRResult getECTYPERResults(Path staramrFilePath) throws IOException, PostProcessingException { 
 	*/
 	private ECTYPERResult getECTYPERResults(Path ectyperreportFilePath) throws IOException, PostProcessingException { 
-		final int OTYPE_INDEX = 2;
-		final int HTYPE_INDEX = 3;
+		final int OTYPE_INDEX = 1;
+		final int HTYPE_INDEX = 2;
 		/*final int DRUG_INDEX = 2;*/
 		final int MAX_TOKENS = 10;
 
@@ -129,11 +129,13 @@ public class ECTyperUpdater implements AnalysisSampleUpdater {
 					ectyperResult.getOtype(), "text", analysis);
 			PipelineProvidedMetadataEntry ectyperHtypeEntry = new PipelineProvidedMetadataEntry(
 					ectyperResult.getHtype(), "text", analysis);
+			PipelineProvidedMetadataEntry ectyperSerotypeEntry = new PipelineProvidedMetadataEntry(
+					ectyperResult.getSerotype(), "text", analysis);
 			
-
-			stringEntries.put(appendVersion("O-type", workflowVersion), ectyperOtypeEntry);
-			stringEntries.put(appendVersion("H-type", workflowVersion), ectyperHtypeEntry);
-			
+			/*Write results to the metadata of project*/
+			stringEntries.put(appendVersion("ECTYPER O-antigen", workflowVersion), ectyperOtypeEntry);
+			stringEntries.put(appendVersion("ECTYPER H-antigen", workflowVersion), ectyperHtypeEntry);
+			stringEntries.put(appendVersion("ECTYPER Serotype",  workflowVersion), ectyperSerotypeEntry);
 
 			Map<MetadataTemplateField, MetadataEntry> metadataMap = metadataTemplateService
 					.getMetadataMap(stringEntries);
@@ -142,7 +144,7 @@ public class ECTyperUpdater implements AnalysisSampleUpdater {
 			sampleService.updateFields(sample.getId(), ImmutableMap.of("metadata", sample.getMetadata()));
 		} catch (IOException e) {
 			logger.error("Got IOException", e);
-			throw new PostProcessingException("Error parsing amr detection results", e);
+			throw new PostProcessingException("Error parsing ectyper results", e);
 		} catch (IridaWorkflowNotFoundException e) {
 			logger.error("Got IridaWorkflowNotFoundException", e);
 			throw new PostProcessingException("Workflow is not found", e);
@@ -160,19 +162,21 @@ public class ECTyperUpdater implements AnalysisSampleUpdater {
 	 * @return The appended name and version.
 	 */
 	private String appendVersion(String name, String version) {
-		return name + "/" + version;
+		return name + "(" + version + ")";
 	}
 
 	/**
-	 * Class used to store together data extracted from the RGI/staramr tables.
+	 * Class used to store together data extracted from the ectyper tab-result files.
 	 */
 	private class ECTYPERResult  {
 		private String otype;
 		private String htype;
+		private String serotype;
 
 		public ECTYPERResult(String otype, String htype) {
 			this.otype = otype;
 			this.htype = htype;
+			this.serotype = serotype;
 		}
 
 		public String getOtype() {
@@ -181,6 +185,10 @@ public class ECTyperUpdater implements AnalysisSampleUpdater {
 
 		public String getHtype() {
 			return htype;
+		}
+
+		public String getSerotype(){
+			return otype+":"+htype;
 		}
 	}
 
